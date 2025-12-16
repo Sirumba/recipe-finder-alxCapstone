efd435638d685beb9b242215cd14f0c2e4802bc9
Sirumba <simonsmaina45@gmail.com>
2025-12-16 21:54:49 +0300

    added the recipecard skeleton structure

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,8 @@
+export default function RecipeCard() {
+  return (
+    <div className="border p-4 rounded-lg shadow-sm">
+      <h2 className="font-bold">Recipe Name</h2>
+      <p>Short description...</p>
+    </div>
+  );
+}
