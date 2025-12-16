1a869fa32cdde45c541c1fa3fb4a61413d1b9314
Sirumba <simonsmaina45@gmail.com>
2025-12-16 21:55:39 +0300

    Added skelton components to the RecipeDetails component

---
+++ src/components/RecipeDetails.jsx
@@ -0,0 +1,8 @@
+export default function RecipeDetails() {
+  return (
+    <div className="p-4 border rounded-lg">
+      <h2 className="text-xl font-bold">Recipe Details</h2>
+      <p>Ingredients & instructions will appear here.</p>
+    </div>
+  );
+}
