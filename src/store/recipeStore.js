1c67db6a3e621a88258391c9cfa38b44efdc2cf6
Sirumba <simonsmaina45@gmail.com>
2025-12-17 20:49:52 +0300

    Set up Zustand store for recipes and UI state

--- src/store/recipeStore.js
+++ src/store/recipeStore.js
@@ -2,13 +2,17 @@ import { create } from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+  isLoading: false,
+  error: null,
   favorites: [],
   selectedRecipe: null,
 
   // Empty functions for now
-  setRecipes: () => {},
-  oggleFavorite: () => {},
-  setSelectedRecipe: () => {},
+  setRecipes: (recipes) => set({ recipes }),
+  setSelectedRecipe: (recipe) => set({ selectedRecipe: recipe}),
+  setLoading: (status) => set({ isLoading: status }),
+  setError: (message) => set({ error: message }),
+  clearError: () => set({ error: null }),
 }));
 
 export default useRecipeStore;
