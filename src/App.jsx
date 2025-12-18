d0e8b37aa8d7174b10deb28efcbbee3c862d2bd7
Sirumba <simonsmaina45@gmail.com>
2025-12-18 20:36:49 +0300

    Render recipe cards from store in main layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import SearchBar from "./components/SearchBar";
 import RecipeCard from "./components/RecipeCard";
+import useRecipeStore from "./store/recipeStore";
 
 export default function App() {
+  const recipes = useRecipeStore((state) => state.recipes);
+  const isLoading = useRecipeStore((state) => state.isLoading);
+  const error = useRecipeStore((state) => state.error);
+
   return (
     <div>
       <Header />
       <SearchBar />
 
-      <div className="p-4 grid gap-4">
-        {/* Temporary demo cards */}
-        <RecipeCard />
-        <RecipeCard />
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
+        {recipes.map((recipe) => (
+          <RecipeCard key={recipe.idMeal} recipe={recipe} />
+        ))}
       </div>
 
       <Footer />
